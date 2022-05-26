1fad22c43a58a41a10da8ccd4dadd6cd42f271c4
hazevich <t.d.chanturia@gmail.com>
2022-05-26 10:47:23 +0200

    setup glfw and glad in premake

--- premake5.lua
+++ premake5.lua
@@ -17,8 +17,16 @@ group "src"
 
         files { "src/LearnOpenGL/**.h", "src/LearnOpenGL/**.cpp" }
 
+        links
+        {
+            "glfw",
+            "glad"
+        }
+
         includedirs
         {
+            "dependencies/glfw/include",
+            "dependencies/glad/include",
             "src/LearnOpenGL"
         }
 
@@ -31,3 +39,64 @@ group "src"
             defines { "RELEASE" }
             runtime "Release"
             optimize "On"
+            group "dependencies"
+            project "glfw"
+                kind "StaticLib"
+                language "C"
+                targetdir (targetdirPath)
+                objdir (objdirPath)
+                location "dependencies/glfw"
+        
+                files
+                {
+                    "dependencies/glfw/src/internal.h",
+                    "dependencies/glfw/src/mappings.h",
+                    "dependencies/glfw/src/context.c",
+                    "dependencies/glfw/src/init.c",
+                    "dependencies/glfw/src/input.c",
+                    "dependencies/glfw/src/monitor.c",
+                    "dependencies/glfw/src/vulkan.c",
+                    "dependencies/glfw/src/window.c",
+                    "dependencies/glfw/include/**.h"
+                }
+        
+                filter "system:windows"
+                    defines 
+                    { 
+                        "_GLFW_WIN32",
+                        "_CRT_SECURE_NO_WARNINGS"
+                    }
+        
+                    files
+                    {
+        
+                        "dependencies/glfw/src/win32_platform.h",
+                        "dependencies/glfw/src/win32_joystick.h",
+                        "dependencies/glfw/src/win32_init.c",
+                        "dependencies/glfw/src/win32_joystick.c",
+                        "dependencies/glfw/src/win32_monitor.c",
+                        "dependencies/glfw/src/win32_time.c",
+                        "dependencies/glfw/src/win32_thread.c",
+                        "dependencies/glfw/src/win32_window.c",
+                        "dependencies/glfw/src/wgl_context.h",
+                        "dependencies/glfw/src/egl_context.h",
+                        "dependencies/glfw/src/osmesa_context.h",
+                        "dependencies/glfw/src/wgl_context.c",
+                        "dependencies/glfw/src/egl_context.c",
+                        "dependencies/glfw/src/osmesa_context.c"
+                    }
+        
+            project "glad"
+                kind "StaticLib"
+                language "C"
+                targetdir (targetdirPath)
+                objdir (objdirPath)
+                location "dependencies/glad"
+        
+                files 
+                {
+                    "dependencies/glad/**.h",
+                    "dependencies/glad/**.c"
+                }
+        
+                includedirs { "dependencies/glad/include" }
