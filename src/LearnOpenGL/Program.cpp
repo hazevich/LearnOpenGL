aa63cad85cd15bbca9d902f1f1f8f4f1ea5e3b5c
hazevich <t.d.chanturia@gmail.com>
2022-05-26 11:13:56 +0200

    Hello world glfw window

--- src/LearnOpenGL/Program.cpp
+++ src/LearnOpenGL/Program.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+void FramebufferResized(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 int main()
 {
-    std::cout << "Hello world";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    const int width = 1280, height = 720;
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "Hello world", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Could not create a window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Could not load GLAD" << std::endl;
+        return 1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, FramebufferResized);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
+
