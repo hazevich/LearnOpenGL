71a1bac8c63edaa70e6bb5f4b388abcf8e71f616
tchanturia <t.d.chanturia@gmail.com>
2023-12-13 21:18:50 +0100

    Hello Triangle - Draw 2 triangles

--- src/LearnOpenGL/LearnOpenGL.cpp
+++ src/LearnOpenGL/LearnOpenGL.cpp
@@ -114,16 +114,26 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-       -0.5f, -0.5f, 0.0f,  // bottom left
-       -0.5f,  0.5f, 0.0f   // top left 
-    };
+    //float vertices[] = {
+    //    0.5f,  0.5f, 0.0f,  // top right
+    //    0.5f, -0.5f, 0.0f,  // bottom right
+    //   -0.5f, -0.5f, 0.0f,  // bottom left
+    //   -0.5f,  0.5f, 0.0f   // top left 
+    //};
 
-    uint32_t indices[] = {
-        0, 1, 3,
-        1, 2, 3,
+    //uint32_t indices[] = {
+    //    0, 1, 3,
+    //    1, 2, 3,
+    //};
+
+    float vertices[] = {
+       -0.5f,  0.5f, 0.0f,
+        0.0f, -0.5f, 0.0f,
+       -1.0f, -0.5f, 0.0f,
+
+        0.5f,  0.5f, 0.0f,
+        1.0f, -0.5f, 0.0f,
+        0.0f, -0.5f, 0.0f,
     };
 
     uint32_t VBO;
@@ -140,15 +150,15 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindVertexArray(0);
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -159,7 +169,8 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
