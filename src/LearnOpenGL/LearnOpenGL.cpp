b820021abbaee564884aeb6d3e3eb0bc3a3780f1
tchanturia <t.d.chanturia@gmail.com>
2023-12-31 17:09:28 +0100

    Camera

--- src/LearnOpenGL/LearnOpenGL.cpp
+++ src/LearnOpenGL/LearnOpenGL.cpp
@@ -13,6 +13,16 @@
 #include "Texture2D.h"
 
 float mixValue = 0.2f;
+float currentFrame = 0.0f;
+float lastFrame = 0.0f;
+float deltaTime = 0.0f;
+
+float yaw = -90.0f;
+float pitch = 0.0f;
+
+glm::vec3 cameraPos(0.0f);
+glm::vec3 cameraFront(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -37,6 +47,63 @@ void processInput(GLFWwindow* window)
     }
 
     mixValue = std::clamp(mixValue, 0.0f, 1.0f);
+
+
+    float cameraSpeed = 2.5f * deltaTime;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        cameraPos += cameraFront * cameraSpeed;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        cameraPos += cameraFront * -1.0f * cameraSpeed;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        cameraPos += glm::normalize(glm::cross(cameraUp, cameraFront)) * cameraSpeed;
+    }
+}
+
+float lastX = 0, lastY = 0;
+bool firstMouse = true;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 10.f;
+    yaw += xoffset * sensitivity * deltaTime;
+    pitch += yoffset * sensitivity * deltaTime;
+
+    if (pitch < -89.f)
+        pitch = -89.f;
+    else if (pitch > 89.f)
+        pitch = 89.f;
+
+    glm::vec3 direction;
+    direction.x = glm::cos(glm::radians(yaw)) * glm::cos(glm::radians(pitch));
+    direction.y = glm::sin(glm::radians(pitch));
+    direction.z = glm::sin(glm::radians(yaw)) * glm::cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
 }
 
 int main()
@@ -65,6 +132,10 @@ int main()
 
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     glEnable(GL_DEPTH_TEST);
 
@@ -163,6 +234,10 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+        currentFrame = float(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -174,10 +249,8 @@ int main()
         glBindTexture(GL_TEXTURE_2D, awesomefaceTexture.TextureId);
 
         shader.SetFloat("mixValue", mixValue);
-
         glm::mat4 view(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -9.0f));
-        view = glm::rotate(view, float(glfwGetTime()) * glm::pi<float>() * glm::radians(10.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
         glm::mat4 projection(1.0f);
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
@@ -194,7 +267,6 @@ int main()
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
-
 
         glBindVertexArray(VAO);
 
