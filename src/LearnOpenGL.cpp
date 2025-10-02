7fe1b127642d6f9ec92df49a20fd894192b03323
tchanturia <t.d.chanturia@gmail.com>
2025-10-02 22:46:02 +0200

    cmake glfw

---
+++ src/LearnOpenGL.cpp
@@ -0,0 +1,13 @@
+﻿// LearnOpenGL.cpp : Defines the entry point for the application.
+//
+
+#include "LearnOpenGL.h"
+#include "GLFW/glfw3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
