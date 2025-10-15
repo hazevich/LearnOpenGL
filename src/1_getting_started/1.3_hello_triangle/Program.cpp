78b4a783137bb174d258cdafeb48d4a9bba3c4c8
tchanturia <t.d.chanturia@gmail.com>
2025-10-15 20:25:35 +0200

    vertex shader

--- src/1_getting_started/1.3_hello_triangle/Program.cpp
+++ src/1_getting_started/1.3_hello_triangle/Program.cpp
@@ -5,6 +5,22 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+float _vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f,
+};
+
+const char* _vertexShader = R"(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)";
+
 int main()
 {
     glfwInit();
@@ -30,6 +46,27 @@ int main()
 
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(_vertices), _vertices, GL_STATIC_DRAW);
+
+    unsigned int vertexShaderId;
+    vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShaderId, 1, &_vertexShader, NULL);
+    glCompileShader(vertexShaderId);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShaderId, sizeof(infoLog), NULL, infoLog);
+        std::cout << "Vertex shader compilation failed\n" << infoLog << std::endl;
+    }
+
 
     while (!glfwWindowShouldClose(window))
     {
