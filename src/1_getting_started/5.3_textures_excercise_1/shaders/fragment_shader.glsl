16e59760103977d2b39f5d314fd4168befbef78b
tchanturia <t.d.chanturia@gmail.com>
2025-10-19 19:59:07 +0200

    5.3 textures excercise 1

---
+++ src/1_getting_started/5.3_textures_excercise_1/shaders/fragment_shader.glsl
@@ -0,0 +1,12 @@
+#version 460 core
+out vec4 FragColor;
+in vec3 vertexColor;
+in vec2 TexCoord;
+
+uniform sampler2D containerTexture;
+uniform sampler2D awesomefaceTexture;
+
+void main()
+{
+    FragColor = mix(texture(containerTexture, TexCoord), texture(awesomefaceTexture, vec2(1.0f - TexCoord.x, TexCoord.y)), 0.2f);
+}
